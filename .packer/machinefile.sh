d7b48e1e20301a2af0233d1d36897eaa680ce9bd
Gerard Braad <me@gbraad.nl>
2025-08-31 07:44:54 +0000

    Add base for packer

--- .packer/machinefile.sh
+++ .packer/machinefile.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-machinefile --host ${SSH_HOST} --port ${SSH_PORT} --user root --password password --arg=USER_PASSWD=${USER_PASSWD} .packer/Machinefile .
+machinefile --host ${SSH_HOST} --port ${SSH_PORT} --user root --password password --arg=USER_PASSWD=${USER_PASSWD} .packer/Machinefile-base .
+machinefile --host ${SSH_HOST} --port ${SSH_PORT} --user root --password password .packer/Machinefile .
+
